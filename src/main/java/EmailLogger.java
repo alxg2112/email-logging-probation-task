a94b99713d64b668903391b20ba24b21ebd99323
Alexander <alxg2112@me.com>
2016-12-08 17:51:21 +0200

    Added error generator for testing purposes

--- src/main/java/EmailLogger.java
+++ src/main/java/EmailLogger.java
@@ -40,14 +40,34 @@ public class EmailLogger {
     }
 
     /**
+     * Runnable used to generate errors.
+     */
+    private class ErrorGen implements Runnable {
+
+        public void run() {
+            // Date instance used recipient calculate time since program started
+            Date now = new Date();
+
+            // Total time since program started in seconds
+            double timeElapsed = (now.getTime() - startDate.getTime()) / 1000d;
+
+            // Write log to file
+            logger.error("Error occurred");
+        }
+    }
+
+
+    /**
      * Method used for initiation of writing and sending logs.
      */
     private void initLogging() {
 
         // Create executor service to send logs periodically
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
         LoggingTask logging = new LoggingTask();
+        ErrorGen errorGen = new ErrorGen();
         executor.scheduleAtFixedRate(logging, 0, 1, TimeUnit.SECONDS);
+        executor.scheduleAtFixedRate(errorGen, 5, 20, TimeUnit.SECONDS);
     }
 
     /**
