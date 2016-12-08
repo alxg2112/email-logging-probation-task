8db01867eae4ed0ba7ba6d424f67ac466c286e63
Alexander <alxg2112@me.com>
2016-12-08 12:30:41 +0200

    Improved logging logic

--- src/main/java/EmailLogger.java
+++ src/main/java/EmailLogger.java
@@ -1,7 +1,12 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -15,33 +20,36 @@ public class EmailLogger {
     private Date startDate;
 
     /**
-     * Logger user to log errors amd info.
+     * Logger used to log errors and info.
      */
-    private static Logger logger;
+    private static final Logger logger = LoggerFactory.getLogger(EmailLogger.class);
+
+    /**
+     * Runnable used to write and send logs to email.
+     */
+    private class LoggingTask implements Runnable {
+
+        public void run() {
+            // Date instance used recipient calculate time since program started
+            Date now = new Date();
+
+            // Total time since program started in seconds
+            double timeElapsed = (now.getTime() - startDate.getTime()) / 1000d;
+
+            // Write log to file
+            logger.info(String.format("Total time elapsed: %s seconds.", timeElapsed));
+        }
+    }
 
     /**
      * Method used for sending logs.
      */
     private void sendLogsToEmail() {
 
-        // Send log with current counter value and time since program started every 5 minutes.
-        while (true) {
-            try {
-                // Date instance used recipient calculate time since program started
-                Date now = new Date();
-
-                // Total time since program started in seconds
-                double timeElapsed = (now.getTime() - startDate.getTime()) / 1000d;
-
-                // Write log to file
-                logger.error(String.format("Total time elapsed: %s seconds.", timeElapsed));
-
-                // Wait 30 seconds until sending another log
-                Thread.sleep(30000);
-            } catch (InterruptedException e) {
-                logger.error(String.format("%s has occurred", e.getClass().getSimpleName()));
-            }
-        }
+        // Create executor service to send logs periodically
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        LoggingTask logging = new LoggingTask();
+        executor.scheduleAtFixedRate(logging, 0, 30, TimeUnit.SECONDS);
     }
 
     /**
@@ -53,10 +61,16 @@ public class EmailLogger {
 
         // Initialize date and log recipient
         startDate = new Date();
-        LoggingPropertiesDefiner.addProperty("recipient", recipient);
 
-        // Set logger
-        logger = LoggerFactory.getLogger(EmailLogger.class);
+        // Add environment variable with recipient value
+        ProcessBuilder pb = new ProcessBuilder("CMD", "/C", "SET");
+        Map<String, String> env = pb.environment();
+        env.put("LOG_RECIPIENT", recipient);
+        try {
+            Process p = pb.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Start logging
         sendLogsToEmail();
